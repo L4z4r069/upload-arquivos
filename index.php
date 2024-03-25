e46ae24d29d3a92168a540b3d1e3e6b11d023156
L4z4r069 <lazaro.2022315968@aluno.iffar.edu.br>
2024-03-25 16:32:50 -0300

    index pronto

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        selecione o arquivo:<br><br>    
+        <input type="file" name="arquivo"><br><br>
+        <input type="submit" value="Fazer Upload" name="submit">
+</form>
+</body>
+</html>
