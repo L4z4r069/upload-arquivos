f5c4c929553ad1dac60965fd431afd63e8ec3f0a
L4z4r069 <lazaro.2022315968@aluno.iffar.edu.br>
2024-04-01 16:23:58 -0300

    sei la

--- upload.php
+++ upload.php
@@ -1,8 +1,24 @@
 <?php
 
-$pastaDestino = "uploads/";
-$nomearquivo = $_FILES['arquivo']['name'];
+// Definiu a pasta de destino
+$pastaDestino = "/uploads/";
+
+// Pegamos o nome do arquivo
+$nomeArquivo = $_FILES['arquivo']['name'];
+
+
 var_dump($_FILES['arquivo']['name']);
 
+// Verificar se o arquivo já existe
+if (file_exists(__DIR__ . $pastaDestino . $nomeArquivo)){
+    echo "Arquivo já existe";
+    exit;
+}
 
+var_dump(__DIR__ . $pastaDestino . $nomeArquivo);
+
+if ($_FILES['arquivo']['size'] > 10000000){ //10M
+    echo "Arquivo muito grande";
+    exit;
+}
 ?>
